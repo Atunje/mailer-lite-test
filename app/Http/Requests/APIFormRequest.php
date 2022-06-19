a1fd2abaa6e3afed5d333a7d9bee0e715abb45f7
Ademuyiwa Adetunji <nobelatunje001@gmail.com>
2022-06-19 23:26:23 +0100

    refactored form requests to avoid duplicated code

---
+++ app/Http/Requests/APIFormRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+use Illuminate\Contracts\Validation\Validator;
+
+abstract class APIFormRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Customize the response when validation fails
+     */
+    protected function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'message' => $validator->errors()
+        ], 422));
+    }
+
+    /**
+     * Ensure child classes implement rules
+     */
+    abstract function rules();
+}
--- app/Http/Requests/SubscriberRequest.php
+++ app/Http/Requests/SubscriberRequest.php
@@ -2,24 +2,11 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Http\Exceptions\HttpResponseException;
-use Illuminate\Contracts\Validation\Validator;
 use App\Models\Field;
 use Illuminate\Validation\Rule;
 
-class SubscriberRequest extends FormRequest
+class SubscriberRequest extends APIFormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize()
-    {
-        return true;
-    }
-
     /**
      * Get the validation rules that apply to the request.
      *
@@ -52,15 +39,6 @@ class SubscriberRequest extends FormRequest
         }
 
         return $rules;
-    }
-
-
-    protected function failedValidation(Validator $validator)
-    {
-        throw new HttpResponseException(response()->json([
-            'status' => false,
-            'message' => $validator->errors()
-        ], 422));
     }
 
 }
