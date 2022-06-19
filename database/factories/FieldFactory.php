2f53c654b98f58cd53e9b10df250a734c3dada24
Ademuyiwa Adetunji <nobelatunje001@gmail.com>
2022-06-19 16:34:18 +0100

    created different states on field factory

--- database/factories/FieldFactory.php
+++ database/factories/FieldFactory.php
@@ -17,8 +17,54 @@ class FieldFactory extends Factory
     public function definition()
     {
         return [
-            'title' => $this->faker->name(),
+            'title' => $this->faker->randomElement(['address', 'location', 'company', 'phone_number', 'next_of_kin', 'city', 'school']),
             'type' => 'string',
         ];
+    }
+
+    /**
+     * Indicate that the type of the field as date.
+     *
+     * @return static
+     */
+    public function date()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'title' => $this->faker->randomElement(['date_of_birth', 'arrival_date', 'departure_date', 'registration_date', 'next_transaction_date', 'date', 'entry_date']),
+                'type' => 'date',
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the type of the field as number.
+     *
+     * @return static
+     */
+    public function number()
+    {
+        return $this->state(function (array $attributes) {
+            return [
+                'title' => $this->faker->randomElement(['amount', 'no_of_children', 'years_of_experience', 'worth', 'cars', 'houses']),
+                'type' => 'number',
+            ];
+        });
+    }
+
+    /**
+     * Indicate that the type of the field is boolean
+     *
+     * @return static
+     */
+    public function boolean()
+    {
+        
+        return $this->state(function (array $attributes) {
+            return [
+                'title' => $this->faker->randomElement(['verified', 'suspended', 'graded', 'employed', 'paid']),
+                'type' => 'boolean',
+            ];
+        });
     }
 }
